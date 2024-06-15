25ed51ab624bf08892bdc488f9948a1da1e591aa
chadireoroonu <chadireoroonu@gmail.com>
2024-06-15 22:54:24 +0900

    🎉 Init: curd 연습 프로젝트 시작

---
+++ crud1/src/test/java/org/example/crud1/Crud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
